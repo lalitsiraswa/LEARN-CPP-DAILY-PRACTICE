2691cc1847589d9ecbbdcb7369bf36604f70d91e
Lavik <lavik@Laviks-MacBook-Pro.local>
2026-03-19 11:47:22 +0530

    Inital Project Setup

---
+++ Learning-CPP-Daily-Practice/Learning-CPP-Daily-Practice/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Learning-CPP-Daily-Practice
+//
+//  Created by Lavik on 19/03/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
