b19b6ef60dddad8a537fa836ea3a659c364f369b
Lavik <lavik@Laviks-MacBook-Pro.local>
2026-04-07 21:29:59 +0530

    78. Subsets

--- Keep-Revising-Practice/Keep-Revising-Practice/AdityaVermaRecursionPractice.cpp
+++ Keep-Revising-Practice/Keep-Revising-Practice/AdityaVermaRecursionPractice.cpp
@@ -140,9 +140,38 @@ void subSets(string ip, string op) {
     return;
 }
 
+// -------------------------------------------------- 78. Subsets --------------------------------------------------
+void subsets(vector<vector<int>> &result, vector<int> &subset, int index, vector<int> &nums) {
+    if(index == nums.size()){
+        result.push_back(subset);
+        return;
+    }
+    // Not Pick
+    subsets(result, subset, index+1, nums);
+    // Pick
+    subset.push_back(nums[index]);
+    subsets(result, subset, index+1, nums);
+    subset.pop_back();
+}
+
+vector<vector<int>> subsets(vector<int>& nums) {
+    vector<vector<int>> result;
+    vector<int> subset;
+    subsets(result, subset, 0, nums);
+    return result;
+}
+
 int main(){
-    string ip = "abc";
-    string op = "";
-    subSets(ip, op);
+    vector<int> nums = {1, 2, 3};
+    vector<vector<int>> result = subsets(nums);
+    for (auto it = result.begin(); it != result.end(); ++it) {
+        cout << "{";
+        for (auto jt = it->begin(); jt != it->end(); ++jt) {
+            cout << *jt;
+            if (next(jt) != it->end())
+                cout << ", ";
+        }
+        cout << "}" << endl;
+    }
     return 0;
 }
--- Learning-CPP-Daily-Practice/Learning-CPP-Daily-Practice/AdityaVermaRecursionPractice.cpp
+++ Learning-CPP-Daily-Practice/Learning-CPP-Daily-Practice/AdityaVermaRecursionPractice.cpp
@@ -1,24 +1,38 @@
 #include <iostream>
 using namespace std;
 
-// -------------------------------------------------- String Subsets --------------------------------------------------
-void subSets(string ip, string op) {
-    if(ip.empty()){
-        cout << op << endl;
+// -------------------------------------------------- 78. Subsets --------------------------------------------------
+void subsets(vector<vector<int>> &result, vector<int> &subset, int index, vector<int> &nums) {
+    if(index == nums.size()){
+        result.push_back(subset);
         return;
     }
-    string op1 = op;
-    string op2 = op;
-    op2.push_back(ip[0]);
-    ip.erase(ip.begin());
-    subSets(ip, op1);
-    subSets(ip, op2);
-    return;
+    // Not Pick
+    subsets(result, subset, index+1, nums);
+    // Pick
+    subset.push_back(nums[index]);
+    subsets(result, subset, index+1, nums);
+    subset.pop_back();
+}
+
+vector<vector<int>> subsets(vector<int>& nums) {
+    vector<vector<int>> result;
+    vector<int> subset;
+    subsets(result, subset, 0, nums);
+    return result;
 }
 
 int main(){
-    string ip = "abc";
-    string op = "";
-    subSets(ip, op);
+    vector<int> nums = {1, 2, 3};
+    vector<vector<int>> result = subsets(nums);
+    for (auto it = result.begin(); it != result.end(); ++it) {
+        cout << "{";
+        for (auto jt = it->begin(); jt != it->end(); ++jt) {
+            cout << *jt;
+            if (next(jt) != it->end())
+                cout << ", ";
+        }
+        cout << "}" << endl;
+    }
     return 0;
 }
