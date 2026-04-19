9ad65eb95e5bda422b73fd86c261f374d075f13a
Lavik <lavik@Laviks-MacBook-Pro.local>
2026-04-19 18:55:17 +0530

    22. Generate Parentheses

--- Keep-Revising-Practice/Keep-Revising-Practice/AdityaVermaRecursionPractice.cpp
+++ Keep-Revising-Practice/Keep-Revising-Practice/AdityaVermaRecursionPractice.cpp
@@ -259,12 +259,36 @@ vector<string> letterCasePermutation(string s) {
     return result;
 }
 
-int main(){
-    string s = "aBc";
-    vector<string> result = letterCasePermutation(s);
+// -------------------------------------------------- 22. Generate Parentheses --------------------------------------------------
+void generateParenthesis(int openParenthesisCount, int closeParenthesisCount, string &temp, vector<string> &result) {
+    if(openParenthesisCount == 0 && closeParenthesisCount == 0){
+        result.push_back(temp);
+        return;
+    }
+    if(openParenthesisCount != 0){
+        temp.push_back('(');
+        generateParenthesis(openParenthesisCount - 1, closeParenthesisCount, temp, result);
+        temp.pop_back();
+    }
+    if(openParenthesisCount < closeParenthesisCount){
+        temp.push_back(')');
+        generateParenthesis(openParenthesisCount, closeParenthesisCount - 1, temp, result);
+        temp.pop_back();
+    }
+}
+
+vector<string> generateParenthesis(int n) {
+    vector<string> result;
+    string temp;
+    generateParenthesis(n, n, temp, result);
     for(auto item : result){
         cout << item << endl;
     }
+    return result;
+}
+
+int main(){
+    generateParenthesis(2);
     return 0;
 }
 
--- Learning-CPP-Daily-Practice/Learning-CPP-Daily-Practice/AdityaVermaRecursionPractice.cpp
+++ Learning-CPP-Daily-Practice/Learning-CPP-Daily-Practice/AdityaVermaRecursionPractice.cpp
@@ -1,83 +1,35 @@
 #include <iostream>
 using namespace std;
 
-// -------------------------------------------------- 784. Letter Case Permutation --------------------------------------------------
-void stringPermutationWithCaseChange01(string input, string output, vector<string> &result) {
-    if(input.empty()){
-        result.push_back(output);
+// -------------------------------------------------- 22. Generate Parentheses --------------------------------------------------
+void generateParenthesis(int openParenthesisCount, int closeParenthesisCount, string &temp, vector<string> &result) {
+    if(openParenthesisCount == 0 && closeParenthesisCount == 0){
+        result.push_back(temp);
         return;
     }
-    char ip = input[0];
-    string op1 = output;
-    string op2 = output;
-    // Capital Character
-    if(ip >= 65 && ip <= 90){
-        // No case change
-        op1.push_back(ip);
-        // Change case
-        op2.push_back(ip + 32);
-        input.erase(input.begin());
-        stringPermutationWithCaseChange01(input, op1, result);
-        stringPermutationWithCaseChange01(input, op2, result);
+    if(openParenthesisCount != 0){
+        temp.push_back('(');
+        generateParenthesis(openParenthesisCount - 1, closeParenthesisCount, temp, result);
+        temp.pop_back();
     }
-    // Small Character
-    else if(ip >= 97 &&ip <= 122){
-        // No case change
-        op1.push_back(ip);
-        // Change case
-        op2.push_back(ip - 32);
-        input.erase(input.begin());
-        stringPermutationWithCaseChange01(input, op1, result);
-        stringPermutationWithCaseChange01(input, op2, result);
-    }
-    // Numerical
-    else{
-        // No case change
-        op1.push_back(ip);
-        input.erase(input.begin());
-        stringPermutationWithCaseChange01(input, op1, result);
+    if(openParenthesisCount < closeParenthesisCount){
+        temp.push_back(')');
+        generateParenthesis(openParenthesisCount, closeParenthesisCount - 1, temp, result);
+        temp.pop_back();
     }
 }
 
-void stringPermutationWithCaseChange02(string input, string output, vector<string> &result) {
-    if(input.empty()){
-        result.push_back(output);
-        return;
-    }
-    char ip = input[0];
-    string op1 = output;
-    string op2 = output;
-    // Check Digit
-    if(ip >= 48 && ip <= 57){
-        // No case change
-        op1.push_back(ip);
-        input.erase(input.begin());
-        stringPermutationWithCaseChange02(input, op1, result);
-    }
-    else {
-        // Lowercase Scenario
-        op1.push_back(tolower(ip));
-        // Uppercase Scenario
-        op2.push_back(toupper(ip));
-        input.erase(input.begin());
-        stringPermutationWithCaseChange02(input, op1, result);
-        stringPermutationWithCaseChange02(input, op2, result);
-    }
-}
-
-vector<string> letterCasePermutation(string s) {
+vector<string> generateParenthesis(int n) {
     vector<string> result;
-    string output = "";
-    // stringPermutationWithCaseChange01(s, output, result);
-    stringPermutationWithCaseChange02(s, output, result);
+    string temp;
+    generateParenthesis(n, n, temp, result);
+    for(auto item : result){
+        cout << item << endl;
+    }
     return result;
 }
 
 int main(){
-    string s = "aBc";
-    vector<string> result = letterCasePermutation(s);
-    for(auto item : result){
-        cout << item << endl;
-    }
+    generateParenthesis(2);
     return 0;
 }
