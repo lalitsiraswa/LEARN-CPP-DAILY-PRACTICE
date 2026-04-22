d01bc4c0878e6f910099e88fd4369f3fa0a653a7
Lavik <lavik@Laviks-MacBook-Pro.local>
2026-04-22 22:45:47 +0530

    Josephus Problem | Game of Death in a circle | Execution in Circle

--- Learning-CPP-Daily-Practice/Learning-CPP-Daily-Practice/AdityaVermaRecursionPractice.cpp
+++ Learning-CPP-Daily-Practice/Learning-CPP-Daily-Practice/AdityaVermaRecursionPractice.cpp
@@ -1,36 +1,30 @@
 #include <iostream>
 using namespace std;
 
-// -------------------------------------------------- Print N-bit binary numbers having more 1’s than or equal to 0’s for any prefix --------------------------------------------------
-
-void NBitBinary(int n, int onesCount, int zerosCount, string &bitBinary, vector<string> &bitBinaries) {
-    if(n == 0){
-        bitBinaries.push_back(bitBinary);
+// -------------------------------------------------- Josephus Problem | Game of Death in a circle | Execution in Circle --------------------------------------------------
+void josephus(int index, vector<int> vect, int k, int &ans) {
+    if(vect.size() == 1){
+        ans = vect[0];
         return;
     }
-    bitBinary.push_back('1');
-    NBitBinary(n - 1, onesCount + 1, zerosCount, bitBinary, bitBinaries);
-    bitBinary.pop_back();
-    if(onesCount > zerosCount){
-        bitBinary.push_back('0');
-        NBitBinary(n - 1, onesCount, zerosCount + 1, bitBinary, bitBinaries);
-        bitBinary.pop_back();
-    }
+    index = (index + k) % vect.size();
+    vect.erase(vect.begin() + index);
+    josephus(index, vect, k, ans);
 }
-
-vector<string> NBitBinary(int n) {
-    vector<string> bitBinaries;
-    string bitBinary;
-    int onesCount = 0;
-    int zerosCount = 0;
-    NBitBinary(n, onesCount, zerosCount, bitBinary, bitBinaries);
-    for(auto item : bitBinaries){
-        cout << item << endl;
+int josephus(int n, int k) {
+    vector<int> vect; // Person Arrays
+    for(int i = 1; i <= n; i++){
+        vect.push_back(i);
     }
-    return bitBinaries;
+    k = k - 1;
+    int index = 0; // Sward for killing the target person
+    int ans = -1; // To store the number of the last person
+    josephus(index, vect, k, ans);
+    return ans;
 }
 
 int main(){
-    NBitBinary(3);
+//    cout << josephus(40, 7) << endl;
+    cout << josephus(10, 4) << endl;
     return 0;
 }
