3e4b98c22bd2f12ab3b6afa98636144c9347742d
Lavik <lavik@Laviks-MacBook-Pro.local>
2026-08-22 16:02:18 +0530

    minEatingSpeedBruteForce

--- Learning-CPP-Daily-Practice/Learning-CPP-Daily-Practice/KeepPractice.cpp
+++ Learning-CPP-Daily-Practice/Learning-CPP-Daily-Practice/KeepPractice.cpp
@@ -1,6 +1,37 @@
 #include<iostream>
 using namespace std;
 
+// -------------------------------------------- 875. Koko Eating Bananas --------------------------------------------
+int minEatingSpeedBruteForce(vector<int>& piles, int h) {
+    int max_val = *max_element(piles.begin(), piles.end());
+    int k = max_val;
+    for(k; k >= 1; k--){
+        int currHour = 0;
+        for(int pile : piles){
+            currHour += (pile + k - 1) / k;
+//            if(pile <= k){
+//                currHour++;
+//            }
+//            else{
+//                currHour += (pile/k) + 1;
+//            }
+            if(currHour > h){
+                k++;
+                return k;
+            }
+        }
+    }
+    k++;
+    return k;
+}
+
+int main(){
+    vector<int> piles = {3,6,7,11};
+    int h = 8;
+    cout << minEatingSpeedBruteForce(piles, h) << endl;
+    return 0;
+}
+
 // -------------------------------------------- Printing Number in Descending order --------------------------------------------
 
 int findMax(vector<int> &nums, int previousMax){
